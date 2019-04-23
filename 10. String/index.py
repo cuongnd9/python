d7399d474d8aeed5d585ceb47215cb13d0d9f9be
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-04-24 00:16:57 +0700

    String

---
+++ 10. String/index.py
@@ -0,0 +1,13 @@
+print('Python is the best')
+
+# Multiline
+print('''But I
+prefer Node!''')
+
+text = 'Hello Vietnam!'
+print(text[-1])
+print(text[0])
+print(text[1:5])
+print(text[2:-2])
+
+print(text * 3)
