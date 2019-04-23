fcbda10520e6b6a6003b031ea7280696696e35ac
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-04-24 00:13:44 +0700

    Tuples

---
+++ 09. Tuples/index.py
@@ -0,0 +1,8 @@
+# Tuple is similar to a list except you cannot change elements of a tuple once it is defined.
+# Whereas in a list, items can be modified.
+
+language = ("French", "German", "English", "Polish")
+
+print(language[1]) #Output: German
+print(language[3]) #Output: Polish
+print(language[-1]) # Output: Polish
