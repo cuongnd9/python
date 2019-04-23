75f583f09c837619210f6562f53180eb29b5662d
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-04-24 00:51:03 +0700

    Range

---
+++ 13. range()/index.py
@@ -0,0 +1,20 @@
+# range(start, stop, [step])
+
+numbers = range(1, 6)
+
+print(list(numbers)) # Output: [1, 2, 3, 4, 5]
+print(tuple(numbers)) # Output: (1, 2, 3, 4, 5)
+print(set(numbers)) # Output: {1, 2, 3, 4, 5}
+
+# Output: {1: 99, 2: 99, 3: 99, 4: 99, 5: 99} 
+print(dict.fromkeys(numbers, 99))
+
+# Equivalent to: numbers = range(1, 6)
+numbers1 = range(1, 6 , 1)
+print(list(numbers1)) # Output: [1, 2, 3, 4, 5]
+
+numbers2 = range(1, 6, 2)
+print(list(numbers2)) # Output: [1, 3, 5]
+
+numbers3 = range(5, 0, -1)
+print(list(numbers3)) # Output: [5, 4, 3, 2, 1]
