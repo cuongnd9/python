72316e9f245ecce89844a1a7059394b4c0ad35d5
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-04-24 23:50:26 +0700

    if...else

---
+++ 14. if...else Statement/index.py
@@ -0,0 +1,16 @@
+num = 0
+
+if num > 0:
+  print('num > 0')
+elif num < 0:
+  print('num < 0')
+else:
+  print('num = 0')
+
+if True:
+  print('true')
+  print('true')
+  print('true')
+  print('true')
+  print('true')
+  
